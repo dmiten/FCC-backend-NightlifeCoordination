bd6d5fa9464c7ef6379cdde1ae20b7001ded20b4
netlan <netlan@yandex.ru>
2017-08-16 12:54:42 +0700

    Fix password hashing in makepassport and usermodel.

--- server/makepassport.js
+++ server/makepassport.js
@@ -19,13 +19,11 @@ export default function makePassport() {
         session: false
       }, (email, password, done) => {
         User.findOne({ email }, (err, user) => {
-          if (err) {
-            return done(err);
-          }
-          if (!user || !user.checkPassword(password)) {
-            return done(null, false, { message: "no such user or password error" });
-          }
-          return done(null, user);
+          return (err) ?
+              done(err) :
+              (!user || !user.checkPassword(password)) ?
+                  done(null, false, { message: "no such user or password error" }) :
+                  done(null, user);
         });
       })
   );
--- server/usermodel.js
+++ server/usermodel.js
@@ -21,7 +21,7 @@ userSchema.virtual("password")
   this._plainPassword = password;
   if (password) {
     this.salt = crypto.randomBytes(128).toString("base64");
-    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1, 128, "sha1");
+    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1, 128, "sha1").toString("base64");
   } else {
     this.salt = undefined;
     this.passwordHash = undefined;
@@ -34,7 +34,7 @@ userSchema.virtual("password")
 
 userSchema.methods.checkPassword = function (password) {
   return (!password || !this.passwordHash) ? false :
-      crypto.pbkdf2Sync(password, this.salt, 1, 128, "sha1").toString() === this.passwordHash;
+      crypto.pbkdf2Sync(password, this.salt, 1, 128, "sha1").toString("base64") === this.passwordHash;
 };
 
 export default mongoose.model("User", userSchema);
